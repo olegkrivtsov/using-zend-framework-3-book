1cc97bed1d3fe0554a72dc11cd637509a6c01d3f
Oleg Krivtsov <olegkrivtsov@gmail.com>
2016-08-21 09:48:41 +0600

    Fixed some mistakes in doctrine chapter

---
+++ data/contents_ad.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Using Zend Framework 3 - Contents - Adaptive -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3933778336056438"
+     data-ad-slot="4379339122"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
