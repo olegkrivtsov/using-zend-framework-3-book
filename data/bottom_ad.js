024e544b5f7db1cd2b5c3c1b49a5efec49c091bb
Oleg Krivtsov <olegkrivtsov@gmail.com>
2017-06-25 21:49:10 +0600

    Writing chapter roles

--- data/bottom_ad.js
+++ data/bottom_ad.js
@@ -1,10 +1,6 @@
-﻿<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Using Zend Framework 3 Chapter - Bottom - Adaptive -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3933778336056438"
-     data-ad-slot="1546211126"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+﻿<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="WWATBCMWZJXS2">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
+</form>
